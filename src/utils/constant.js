720144795243ea2996a8fb2dfbca6414bf9b74f8
Vijay Kumar Modak <vijaymodak9945@gmail.com>
2025-07-20 14:22:10 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,9 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+const BASE_URL = import.meta.env.PROD
+  ? "https://vercel.com/vijays-projects-2b8b3162/job-dhundo-backend/snEPzJREDMaU7fhGDwuoXa8i6Rqk"
+  : "http://localhost:8000";
+
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+  `${BASE_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
