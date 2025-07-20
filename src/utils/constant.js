bdd5149b722ccc04a724508454c7a71ce8737697
Vijay Kumar Modak <vijaymodak9945@gmail.com>
2025-07-21 00:32:14 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,6 @@
-const BASE_URL =  "https://jobdhundo-backend-33a9.onrender.com"
+const BASE_URL =  process.env.NODE_ENV === "development"
+  ? "http://localhost:8000"
+  : "https://jobdhundo-backend-33a9.onrender.com";
 
 export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
